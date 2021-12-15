aec2103484db198c53e65059d6e99baffbd3d419
Abraão Brito <abraaobritof10@gmail.com>
2021-12-15 18:48:54 -0400

    dia 2

---
+++ projetob/03-desafio.py
@@ -0,0 +1,6 @@
+nome = input('digite seu nome: ')
+idade = int(input('digite sua idade: '))
+peso = float(input('digite seu peso: '))
+
+print('olá, {}. Hoje você tem {} anos de idade e está pesando {}kg.'.format(
+    nome, idade, peso))
---
+++ projetob/05-operadores-logico.py
@@ -0,0 +1,9 @@
+a = 10
+b = 5
+expression = a == b or b == a
+
+print(a == b or a == b)
+print(a == b and a == b)
+print('o' in 'abraao')
+print(expression)
+print(not expression)
