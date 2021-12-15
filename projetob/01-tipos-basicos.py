eb952f7b56b941023bb9c280674132696bb7d928
Abraão Brito <abraaobritof10@gmail.com>
2021-12-15 00:01:12 -0400

    Initial commit

---
+++ projetob/01-tipos-basicos.py
@@ -0,0 +1,31 @@
+# inteiro
+idade = 26
+idade2: int = 25
+
+# flutuante
+peso = 76.4
+
+# string
+nome = 'Abraão Brandão'
+
+# bool
+casado = True
+
+# interpolacao
+inter1 = f'nome: {nome} - casado? {casado}'
+inter2 = 'nome: {} - casado? {}'.format(nome, casado)
+
+print(50*'-')
+print(idade)
+print(peso)
+print(nome)
+print(casado)
+print(50*'-')
+print(type(idade))
+print(type(peso))
+print(type(nome))
+print(type(casado))
+print(50*'-')
+print(inter1)
+print(inter2)
+print(50*'-')
---
+++ projetob/02-operadores-aritmeticos.py
@@ -0,0 +1,19 @@
+a = 10
+b = 10
+
+# soma
+soma = a + b
+
+# subtracao
+subtracao = a - b
+
+# multiplicacao
+multiplicacao = a * b
+
+# divisao
+divisao = a / b
+
+print('{} + {} = {}'.format(a, b, soma))
+print('{} - {} = {}'.format(a, b, subtracao))
+print('{} * {} = {}'.format(a, b, multiplicacao))
+print('{} / {} = {}'.format(a, b, divisao))
